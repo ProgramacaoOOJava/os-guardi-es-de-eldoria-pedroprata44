7a4a2483652ab2f3942105dd1b66d33a1dcc0220
Pedro Prata <contato.pedroprata@gmail.com>
2026-07-16 01:49:26 +0000

    Desafio Nível Novato

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Instâncias do molde Personagem
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Kratos", "Guerreiro", 10, 200, 100.9);
 
-        .
-        .
-        .
-
+        // Exibe as propriedades de cada instância
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,32 @@
+// Molde para Personagem
 public class Personagem {
+    // Propriedades
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
+    // Construtor
+    // This.propriedade da instância
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
+    // Método para exibir propriedades com formatação
     public void exibirStatus() {
+        System.out.println();
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de vida: " + pontosDeVida);
+        System.out.println("Poder base: " + poderBase);
+        System.out.println();
     }
 }
 
